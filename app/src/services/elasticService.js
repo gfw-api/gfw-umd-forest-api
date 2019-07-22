e20ac7007ab4fa8c7c856af1ffb532f90c7de93f
Adam J. Pain <aj.pain89@gmail.com>
2019-07-22 16:27:04 +0200

    Hacks together new sql, blindly adds new parsing functions

--- app/src/services/elasticService.js
+++ app/src/services/elasticService.js
@@ -16,17 +16,18 @@ const getLocationVars = ({ adm1, adm2 }) => {
     return `iso${adm1 ? `, adm1` : ''}${adm2 ? `, adm2` : ''},`;
 };
 
-const getAreaType = (polyname, version) => {
-    const poly_version = version === '2.8' ? 'gadm28' : 'admin';
-    return `${polyname !== `${poly_version}` ? 'area_poly_aoi' : `area_${poly_version}`}`;
-};
-
-const QUERY = `SELECT {vars} area_extent AS extent2010, area_extent_2000 AS extent2000, \
-                {area_type} AS area, year_data AS loss_data, area_gain AS gain \
+const YEAR_QUERY = `SELECT {vars} year_data.year as year, SUM(year_data.area_loss) as area_loss, \
+                SUM(year_data.carbon_emissions) as emissions, \
                 FROM data
                 WHERE {location} \
-                AND thresh = {threshold} \
-                AND polyname = '{polyname}'`;
+                AND thresh = {threshold}`;
+
+const BASE_QUERY = `SELECT {vars} SUM(total_area) AS area, SUM(total_gain) AS gain, \
+                SUM(extent_2000) AS extent2000, SUM(extent_2010) AS extent2010, \
+                SUM(weighted_biomass_per_ha) AS biomass_density \
+                FROM data
+                WHERE {location} \
+                AND thresh = {threshold}`;
 
 var deserializer = function (obj) {
     return function (callback) {
@@ -40,8 +41,6 @@ class ElasticService {
     //     sql = sql.replace('{location}', getLocationString(params))
     //              .replace('{vars}', getLocationVars(params))
     //              .replace('{threshold}', params.thresh)
-    //              .replace('{area_type}', getAreaType(params.polyname, params.gadm))
-    //              .replace('{polyname}', params.polyname);
     //     let url = '';
     //     let id = '';
     //     if (params.gadm && params.gadm === '2.8') {
@@ -66,8 +65,6 @@ class ElasticService {
         const sql = sqlTemplate.replace('{location}', getLocationString(params))
             .replace('{vars}', getLocationVars(params))
             .replace('{threshold}', params.thresh)
-            .replace('{area_type}', getAreaType(params.polyname, params.gadm))
-            .replace('{polyname}', params.polyname);
         logger.debug('Obtaining data with:', sql);
         const tableId = params.gadm === '2.8' ? config.get('elasticTable.v2') : config.get('elasticTable.v3');
         try {
@@ -88,14 +85,14 @@ class ElasticService {
         return a + b;
     }
 
-    static getLossTotal(data, periods) {
+    static getLossTotals(data, key, periods) {
         let loss = data.map(obj => {
             const filteredLoss =
                 periods ? obj.loss_data.filter(year => year.year >= periods[0] && year.year <= periods[1])
                     : obj.loss_data;
             let lossTotal = filteredLoss
                 .map(year => {
-                    return year.area_loss;
+                    return year[key];
                 });
             return lossTotal.reduce(ElasticService.sum, 0);
         });
@@ -111,7 +108,8 @@ class ElasticService {
                 .map(year => {
                     let tmp = {
                         year: year.year,
-                        value: year.area_loss
+                        loss: year.area_loss,
+                        emissions: year.emissions
                     };
                     return tmp;
                 });
@@ -121,6 +119,7 @@ class ElasticService {
                     tmp[el.year] = 0;
                 }
                 tmp[el.year] += el.value;
+                // emissions
             });
             return tmp;
         });
@@ -139,7 +138,9 @@ class ElasticService {
                 return {
                     year: el[0],
                     loss: el[1],
+                    emissions: 0
                     lossPerc: 100 * el[1] / area
+                    emissionsPerc: 100 * 0 / area
                 };
             });
             return returnData;
@@ -154,6 +155,7 @@ class ElasticService {
             extent2000Perc: 0,
             extent2010: 0,
             extent2010Perc: 0,
+            emissions: 0,
             gain: 0,
             gainPerc: 0,
             loss: 0,
@@ -167,7 +169,8 @@ class ElasticService {
             returnData.gain += d.gain;
             returnData.areaHa += d.area;
         });
-        returnData.loss = ElasticService.getLossTotal(data, periods);
+        returnData.loss = ElasticService.getLossTotals(data, 'area_loss', periods);
+        returnData.emissions = ElasticService.getLossTotals(data, 'emissions', periods);
         returnData.extent2000Perc = 100 * returnData.extent2000 / returnData.areaHa;
         returnData.extent2010Perc = 100 * returnData.extent2010 / returnData.areaHa;
         returnData.gainPerc = 100 * returnData.gain / returnData.areaHa;
@@ -176,14 +179,22 @@ class ElasticService {
     }
 
     * fetchData(params) {
-        const data = yield ElasticService.getData(QUERY, params);
-        if (!data || !data.data || data.data.length === 0) {
+        const base_data = yield ElasticService.getData(BASE_QUERY, params);
+        if (!base_data || !base_data.data || base_data.data.length === 0) {
             logger.error('No data found.');
-            return [];
+            data = []
         }
+        const year_data = yield ElasticService.getData(YEAR_QUERY, params);
+        if (!year_data || !year_data.data || year_data.data.length === 0) {
+            logger.error('No data found.');
+            data = []
+        }
+
+        ///// data = parse(base_data + year_data)
+
         const periodsYears = params.period.length ? [params.period[0].slice(0, 4), params.period[1].slice(0, 4)] : null;
         if (data && Object.keys(data).length > 0) {
-            const totals = ElasticService.getTotals(data.data, periodsYears);
+            const totals = ElasticService.getTotals(year_data.data, periodsYears);
             const returnData = Object.assign({
                 totals,
                 years: ElasticService.getLossByYear(data.data, totals.areaHa, periodsYears)
