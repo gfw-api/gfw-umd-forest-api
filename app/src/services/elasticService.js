80188d7cc591b055d0c1e95e4a5453c7878d7dc2
Adam J. Pain <aj.pain89@gmail.com>
2019-07-24 11:25:00 +0200

    Parse data

--- app/src/services/elasticService.js
+++ app/src/services/elasticService.js
@@ -20,7 +20,8 @@ const YEAR_QUERY = `SELECT {vars} year_data.year as year, SUM(year_data.area_los
                 SUM(year_data.carbon_emissions) as emissions, \
                 FROM data
                 WHERE {location} \
-                AND threshold = {threshold}`;
+                AND threshold = {threshold} \
+                GROUP BY nested(year_data.year)`;
 
 const BASE_QUERY = `SELECT {vars} SUM(total_area) AS area, SUM(total_gain) AS gain, \
                 SUM(extent_2000) AS extent2000, SUM(extent_2010) AS extent2010, \
@@ -37,146 +38,102 @@ var deserializer = function (obj) {
 
 class ElasticService {
     // use this for testing locally
-    // static * getData(sql, params) {
-    //     sql = sql.replace('{location}', getLocationString(params))
-    //              .replace('{vars}', getLocationVars(params))
-    //              .replace('{threshold}', params.thresh)
-    //     console.log(`\n\n\n---SQL-----\n${sql}\n\n\n`);
-
-    //     let url = '';
-    //     let id = '';
-    //     if (params.gadm && params.gadm === '2.8') {
-    //         id = config.get('elasticTable.v2');
-    //         url = `https://production-api.globalforestwatch.org/v1/query/${id}?sql=`;
-    //     } else if (params.gadm && params.gadm === '3.6') {
-    //         id = config.get('elasticTable.v3');
-    //         url = `https://production-api.globalforestwatch.org/v1/query/${id}?sql=`;
-    //     }
-    //     logger.debug('Obtaining data with:', url+sql);
-    //     let result = yield request.get(url+sql);
-    //     if (result.statusCode !== 200) {
-    //         console.error('Error obtaining data:');
-    //         console.error(result);
-    //         return null;
-    //     }
-    //     return JSON.parse(result.body);
-    // }
+    static * getData(sql, params) {
+        sql = sql.replace('{location}', getLocationString(params))
+                 .replace('{vars}', getLocationVars(params))
+                 .replace('{threshold}', params.thresh)
+        let url = '';
+        let id = '';
+        if (params.gadm && params.gadm === '2.8') {
+            id = config.get('elasticTable.v2');
+            url = `https://production-api.globalforestwatch.org/v1/query/${id}?sql=`;
+        } else if (params.gadm && params.gadm === '3.6') {
+            id = config.get('elasticTable.v3');
+            url = `https://production-api.globalforestwatch.org/v1/query/${id}?sql=`;
+        }
+        logger.debug('Obtaining data with:', url+sql);
+        let result = yield request.get(url+sql);
+        if (result.statusCode !== 200) {
+            console.error('Error obtaining data:');
+            console.error(result);
+            return null;
+        }
+        return JSON.parse(result.body);
+    }
 
     // Use this one for prod/staging
-    static* getData(sqlTemplate, params) {
-        const sql = sqlTemplate.replace('{location}', getLocationString(params))
-            .replace('{vars}', getLocationVars(params))
-            .replace('{threshold}', params.thresh)
-        logger.debug('Obtaining data with:', sql);
-        const tableId = params.gadm === '2.8' ? config.get('elasticTable.v2') : config.get('elasticTable.v3');
-        try {
-            const result = yield MicroServiceClient.requestToMicroservice({
-                uri: `/query/${tableId}?sql=${sql}`,
-                method: 'GET',
-                json: true
-            });
-            logger.debug(result);
-            return result.body;
-        } catch (err) {
-            logger.error(err);
-            throw err;
-        }
-    }
+    // static* getData(sqlTemplate, params) {
+    //     const sql = sqlTemplate.replace('{location}', getLocationString(params))
+    //         .replace('{vars}', getLocationVars(params))
+    //         .replace('{threshold}', params.thresh)
+    //     logger.debug('Obtaining data with:', sql);
+    //     const tableId = params.gadm === '2.8' ? config.get('elasticTable.v2') : config.get('elasticTable.v3');
+    //     try {
+    //         const result = yield MicroServiceClient.requestToMicroservice({
+    //             uri: `/query/${tableId}?sql=${sql}`,
+    //             method: 'GET',
+    //             json: true
+    //         });
+    //         logger.debug(result);
+    //         return result.body;
+    //     } catch (err) {
+    //         logger.error(err);
+    //         throw err;
+    //     }
+    // }
 
     static sum(a, b) {
         return a + b;
     }
 
-    static getLossTotals(data, key, periods) {
-        let loss = data.map(obj => {
-            const filteredLoss =
-                periods ? obj.loss_data.filter(year => year.year >= periods[0] && year.year <= periods[1])
-                    : obj.loss_data;
-            let lossTotal = filteredLoss
-                .map(year => {
-                    return year[key];
-                });
-            return lossTotal.reduce(ElasticService.sum, 0);
+    static getYearTotals(data, periods) {
+        const filtered = periods ? data.filter(year => year.year >= periods[0] && year.year <= periods[1]) : data;
+        let tmp = {
+            loss: 0,
+            emissions: 0
+        };
+        filtered.forEach(el => {
+            tmp['loss'] += el.area_loss;
+            tmp['emissions'] += el.emissions;
         });
-        return loss.reduce(ElasticService.sum, 0);
+        return tmp;
     }
 
-    static getLossByYear(data, area, periods) {
-        let loss = data.map(obj => {
-            const filteredLoss =
-                periods ? obj.loss_data.filter(year => year.year >= periods[0] && year.year <= periods[1])
-                    : obj.loss_data;
-            let lossTotal = filteredLoss
-                .map(year => {
-                    let tmp = {
-                        year: year.year,
-                        loss: year.area_loss,
-                        emissions: year.emissions
-                    };
-                    return tmp;
-                });
-            let tmp = {};
-            lossTotal.forEach(el => {
-                if (!tmp[el.year]) {
-                    tmp[el.year] = 0;
-                }
-                tmp[el.year] += el.value;
-                // emissions
-            });
-            return tmp;
+    static getLossByYear(data, periods) {
+        console.log(`\n\n\n---func2 data-----\n${JSON.stringify(data)}\n\n\n`);
+        const year_array = data.year_data;
+        const filtered = periods ? year_array.filter(year => year.year >= periods[0] && year.year <= periods[1]) : year_array;   
+        const parsedData = year_array.map(el => {
+            return {
+                year: el.year,
+                loss: el.area_loss,
+                emissions: el.area_loss,
+                lossPerc: 100 * el.area_loss / data.area,
+                emissionsPerc: 100 * el.emissions / data.area
+            };
         });
-        let returnData = {};
-        let yearKeys = Object.keys(loss[0]);
-        if (yearKeys && yearKeys.length > 0) {
-            yearKeys.forEach(k => {
-                if (!returnData[k]) {
-                    returnData[k] = 0;
-                }
-                loss.forEach(el => {
-                    returnData[k] += el[k];
-                });
-            });
-            returnData = Object.entries(returnData).map(el => {
-                return {
-                    year: el[0],
-                    loss: el[1],
-                    emissions: 0,
-                    lossPerc: 100 * el[1] / area,
-                    emissionsPerc: 100 * 0 / area
-                };
-            });
-            return returnData;
-        } else {
-            return null;
-        }
+        console.log(`\n\n\n---parsedData-----\n${JSON.stringify(parsedData)}\n\n\n`);
+        return parsedData
     }
 
     static getTotals(data, periods) {
         const returnData = {
-            extent2000: 0,
-            extent2000Perc: 0,
-            extent2010: 0,
-            extent2010Perc: 0,
-            emissions: 0,
-            gain: 0,
-            gainPerc: 0,
-            loss: 0,
-            lossPerc: 0,
-            areaHa: 0
+            extent2000: data.extent2000,
+            extent2000Perc: 100 * data.extent2000 / data.area,
+            extent2010: data.extent2010,
+            extent2010Perc: 100 * data.extent2010 / data.area,
+            biomass_density: data.biomass_density,
+            gain: data.gain,
+            gainPerc: 100 * data.gain / data.area,
+            areaHa: data.area
         };
 
-        data.forEach(d => {
-            returnData.extent2000 += d.extent2000;
-            returnData.extent2010 += d.extent2010;
-            returnData.gain += d.gain;
-            returnData.areaHa += d.area;
-        });
-        returnData.loss = ElasticService.getLossTotals(data, 'area_loss', periods);
-        returnData.emissions = ElasticService.getLossTotals(data, 'emissions', periods);
-        returnData.extent2000Perc = 100 * returnData.extent2000 / returnData.areaHa;
-        returnData.extent2010Perc = 100 * returnData.extent2010 / returnData.areaHa;
-        returnData.gainPerc = 100 * returnData.gain / returnData.areaHa;
-        returnData.lossPerc = 100 * returnData.loss / returnData.areaHa;
+        const year_totals = ElasticService.getYearTotals(data.year_data, periods);
+        const total_loss = year_totals.loss;
+        const total_emissions = year_totals.emissions;
+        returnData.lossPerc = 100 * total_loss / returnData.areaHa;
+        returnData.emissions = 100 * total_emissions / returnData.areaHa;
+        console.log(`\n\n\n---returnData-----\n${JSON.stringify(returnData)}\n\n\n`);
         return returnData;
     }
 
@@ -191,20 +148,21 @@ class ElasticService {
             logger.error('No data found.');
             data = []
         }
-        console.log(`\n\n\n---BASE-----\n${JSON.stringify(base_data.data)}\n\n\n`);
-        console.log(`\n\n\n---YEAR-----\n${JSON.stringify(year_data.data)}\n\n\n`);
-        console.log(`\n\n\n---HERE!!!!-----\n\n\n`);
-
-
-
-        ///// data = parse(base_data + year_data)
-        const data = {};
+        
+        const data = {
+            year_data: year_data.data,
+            area: base_data.data[0].area,
+            extent2000: base_data.data[0].extent2000,
+            extent2010: base_data.data[0].extent2010,
+            biomass_density: base_data.data[0].biomass_density,
+            gain: base_data.data[0].gain
+        };
         const periodsYears = params.period.length ? [params.period[0].slice(0, 4), params.period[1].slice(0, 4)] : null;
         if (data && Object.keys(data).length > 0) {
-            const totals = ElasticService.getTotals(year_data.data, periodsYears);
+            const totals = ElasticService.getTotals(data, periodsYears);
             const returnData = Object.assign({
                 totals,
-                years: ElasticService.getLossByYear(data.data, totals.areaHa, periodsYears)
+                years: ElasticService.getLossByYear(data, periodsYears)
             }, params);
             return returnData;
         }
