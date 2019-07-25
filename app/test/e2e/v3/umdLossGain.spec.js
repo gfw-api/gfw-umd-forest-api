68f031d9ea93838b3e6c1ca6b03329097bda437a
Adam J. Pain <aj.pain89@gmail.com>
2019-07-25 15:09:17 +0200

    Fix tests permenantly

--- app/test/e2e/v3/umdLossGain.spec.js
+++ app/test/e2e/v3/umdLossGain.spec.js
@@ -1,103 +1,91 @@
-/* eslint-disable no-unused-vars,no-undef */
 const nock = require('nock');
 const chai = require('chai');
 const fs = require('fs');
 const { getTestServer } = require('../test-server');
-
 const should = chai.should();
-
 const requester = getTestServer();
 nock.disableNetConnect();
 nock.enableNetConnect(process.env.HOST_IP);
-
 describe('UMD Losstests', () => {
-
     before(() => {
         if (process.env.NODE_ENV !== 'test') {
             throw Error(`Running the test suite with NODE_ENV ${process.env.NODE_ENV} may result in permanent data loss. Please use NODE_ENV=test.`);
         }
-
         nock.cleanAll();
     });
-
     it('Make a query to a fake level 2 region should return no data', async () => {
         nock(process.env.CT_URL)
-            .get('/v1/query/10e124f0-3703-41e3-8136-492b131d5326')
+            .get('/v1/query/82a317da-1fec-4bd0-b9d6-ebe786b9f269')
             .query({
-                sql: 'SELECT%20iso%2C%20SUM(total_area)%20AS%20area%2C%20SUM(total_gain)%20AS%20gain%2C%20%20SUM(extent_2000)%20AS%20extent2000%2C%20SUM(extent_2010)%20AS%20extent2010%2C%20SUM(weighted_biomass_per_ha)%20AS%20biomass_density%20%20FROM%20data%20WHERE%20iso%3D%27FOO%27%20AND%20adm1%20%3D%201%20AND%20adm2%20%3D%201%20AND%20threshold%20%3D%2030'
+                sql: 'SELECT iso, adm1, adm2, SUM(total_area) AS area, SUM(total_gain) AS gain,                 SUM(extent_2000) AS extent2000, SUM(extent_2010) AS extent2010,                 SUM(weighted_biomass_per_ha) AS biomass_density                 FROM data\n                WHERE iso = \'FOO\' AND adm1 = 1 AND adm2 = 1                 AND threshold = 30'
             })
             .reply(200, {
-                "data": []
-                });
-
+                data: []
+            });
         nock(process.env.CT_URL)
-            .get('/v1/query/10e124f0-3703-41e3-8136-492b131d5326')
+            .get('/v1/query/82a317da-1fec-4bd0-b9d6-ebe786b9f269')
             .query({
-                sql: 'SELECT%20iso%2C%20adm1%2C%20adm2%2C%20year_data.year%20as%20year%2C%20SUM(year_data.area_loss)%20as%20area_loss%2C%20%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20SUM(year_data.carbon_emissions)%20as%20emissions%2C%20%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20FROM%20data%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20WHERE%20iso%20%3D%20%27FOO%27%0AAND%20adm1%20%3D%201%0AAND%20adm2%20%3D%201%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20AND%20threshold%20%3D%20%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20GROUP%20BY%20nested(year_data.year)'
+                sql: 'SELECT iso, adm1, adm2, year_data.year as year, SUM(year_data.area_loss) as area_loss,                 SUM(year_data.carbon_emissions) as emissions,                 FROM data\n                WHERE iso = \'FOO\' AND adm1 = 1 AND adm2 = 1                 AND threshold = 30                 GROUP BY nested(year_data.year)'
             })
             .reply(500, {});
-
         const response = await requester
             .get(`/api/v3/umd-loss-gain/admin/foo/1/1`)
             .send();
-
         response.status.should.equal(200);
         response.body.should.have.property('data').and.be.an('array').and.length(0);
-
     });
-
     it('Make a query to a real level 1 region should return ...', async () => {
-        const jsonResponse = JSON.parse(fs.readFileSync(`${__dirname}/../data/BRA.json`, 'utf8'));
+        const jsonResponseBase = JSON.parse(fs.readFileSync(`${__dirname}/../data/BRA_base.json`, 'utf8'));
+        const jsonResponseYear = JSON.parse(fs.readFileSync(`${__dirname}/../data/BRA_year.json`, 'utf8'));
+        nock(process.env.CT_URL)
+            .get('/v1/query/82a317da-1fec-4bd0-b9d6-ebe786b9f269')
+            .query({
+                sql: 'SELECT iso, adm1, adm2, SUM(total_area) AS area, SUM(total_gain) AS gain,                 SUM(extent_2000) AS extent2000, SUM(extent_2010) AS extent2010,                 SUM(weighted_biomass_per_ha) AS biomass_density                 FROM data\n                WHERE iso = \'BRA\' AND adm1 = 1 AND adm2 = 1                 AND threshold = 30'
+            })
+            .reply(200, jsonResponseBase);
 
         nock(process.env.CT_URL)
-            .get('/v1/query/10e124f0-3703-41e3-8136-492b131d5326')
-            .query({ sql: 'SELECT%20iso%2C%20adm1%2C%20adm2%2C%20year_data.year%20as%20year%2C%20SUM(year_data.area_loss)%20as%20area_loss%2C%20%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20SUM(year_data.carbon_emissions)%20as%20emissions%2C%20%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20FROM%20data%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20WHERE%20iso%20%3D%20%27BRA%27%0AAND%20adm1%20%3D%201%0AAND%20adm2%20%3D%201%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20AND%20threshold%20%3D%20%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20GROUP%20BY%20nested(year_data.year)' })
-            .reply(200, jsonResponse);
-
-        nock(process.env.CT_URL)
-            .get('/v1/query/10e124f0-3703-41e3-8136-492b131d5326')
-            .query({ sql: "SELECT iso, SUM(total_area) AS area, SUM(total_gain) AS gain,  SUM(extent_2000) AS extent2000, SUM(extent_2010) AS extent2010, SUM(weighted_biomass_per_ha) AS biomass_density  FROM data WHERE iso='BRA' AND adm1 = 1 AND adm2 = 1 AND threshold = 30" })
-            .reply(200, jsonResponse);
-
+            .get('/v1/query/82a317da-1fec-4bd0-b9d6-ebe786b9f269')
+            .query({
+                sql: 'SELECT iso, adm1, adm2, year_data.year as year, SUM(year_data.area_loss) as area_loss,                 SUM(year_data.carbon_emissions) as emissions,                 FROM data\n                WHERE iso = \'BRA\' AND adm1 = 1 AND adm2 = 1                 AND threshold = 30                 GROUP BY nested(year_data.year)'
+            })
+            .reply(200, jsonResponseYear);
         const response = await requester
             .get(`/api/v3/umd-loss-gain/admin/BRA/1/1`)
             .send();
-
+        console.log(`\n\n\n\n RESPONSE ${JSON.stringify(response)}`)
         response.status.should.equal(200);
         response.body.should.have.property('data').and.be.an('object');
         response.body.data.should.have.property('attributes').and.be.an('object');
         response.body.data.should.have.property('id').and.equal('undefined');
         response.body.data.should.have.property('type').and.equal('umd-loss-gain');
-
         response.body.data.attributes.should.have.property('gadm').and.equal('3.6');
         response.body.data.attributes.should.have.property('period').and.be.an('array').and.length(0);
         response.body.data.attributes.should.have.property('years').and.be.an('array').and.length(18);
         response.body.data.attributes.should.have.property('iso').and.equal('BRA');
-        response.body.data.attributes.should.have.property('polyname').and.equal('admin');
+        response.body.data.attributes.should.have.property('adm1').and.equal('1');
+        response.body.data.attributes.should.have.property('adm2').and.equal('1');
         response.body.data.attributes.should.have.property('thresh').and.equal('30');
         response.body.data.attributes.should.have.property('totals').and.be.an('object');
-
-        response.body.data.attributes.totals.should.have.property('areaHa').and.equal(850035649.7393854);
-        response.body.data.attributes.totals.should.have.property('extent2000').and.equal(519187508.445711);
-        response.body.data.attributes.totals.should.have.property('extent2000Perc').and.equal(61.07832166861355);
-        response.body.data.attributes.totals.should.have.property('extent2010').and.equal(498198381.95378244);
-        response.body.data.attributes.totals.should.have.property('extent2010Perc').and.equal(58.60911623018709);
-        response.body.data.attributes.totals.should.have.property('gain').and.equal(7586758.106451291);
-        response.body.data.attributes.totals.should.have.property('gainPerc').and.equal(0.8925223440660794);
+        response.body.data.attributes.totals.should.have.property('areaHa').and.equal(158570.5076035058);
+        response.body.data.attributes.totals.should.have.property('extent2000').and.equal(122284.80057176348);
+        response.body.data.attributes.totals.should.have.property('extent2000Perc').and.equal(77.11698878932006);
+        response.body.data.attributes.totals.should.have.property('extent2010').and.equal(92075.59489374634);
+        response.body.data.attributes.totals.should.have.property('extent2010Perc').and.equal(58.066027715554);
+        response.body.data.attributes.totals.should.have.property('gain').and.equal(440.82270197906);
+        response.body.data.attributes.totals.should.have.property('gainPerc').and.equal(0.2779979131310506);
         response.body.data.attributes.totals.should.have.property('gladAlerts').and.equal(null);
-        response.body.data.attributes.totals.should.have.property('biomassDensity').and.equal(118326137296.57202);
-        response.body.data.attributes.totals.should.have.property('loss').and.equal(53837550.56402553);
-        response.body.data.attributes.totals.should.have.property('lossPerc').and.equal(6.333563842943731);
-        response.body.data.attributes.totals.should.have.property('emissions').and.equal(18308559246.055298);
-        response.body.data.attributes.totals.should.have.property('emissionsPerc').and.equal(2153.857811924543);
+        response.body.data.attributes.totals.should.have.property('biomassDensity').and.equal(33080613.697113037);
+        response.body.data.attributes.totals.should.have.property('loss').and.equal(57399.41572179645);
+        response.body.data.attributes.totals.should.have.property('lossPerc').and.equal(36.19803996927322);
+        response.body.data.attributes.totals.should.have.property('emissions').and.equal(28566265.504786015);
+        response.body.data.attributes.totals.should.have.property('emissionsPerc').and.equal(18014.866658694136);
     });
-
     afterEach(() => {
         if (!nock.isDone()) {
             throw new Error(`Not all nock interceptors were used: ${nock.pendingMocks()}`);
         }
     });
-
     after(() => {
     });
 });
